762a34068e21cdf13369ecf085b872e809e455db
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2022-04-04 20:18:25 -0500

    Update cuenta de nucleotidos.py
    Se agrego el encabezado

--- src/cuenta de nucleotidos.py
+++ src/cuenta de nucleotidos.py
@@ -1,3 +1,22 @@
+#'''NAME
+#cuenta de nucleotidos
+#VERSION
+#1.1
+#AUTHOR
+#Andres Rivera Ramirez
+
+#DESCRIPTION
+#Se cuenta la repetición de cada nucleotido en una secuencia dada por el usuario a traves del teclado
+#CATEGORY
+
+#USAGE
+#Se lee la secuencia desde el teclado, esta es introducida por el usario
+#ARGUMENTS
+
+#SEE ALSO
+
+
+#'''
 #La secuencia de DNA se lee desde el teclado
 dna= input('Inserte secuencia de DNA:')
 #Se utilizan las llaves para hacer varias count en una sola línea de código.

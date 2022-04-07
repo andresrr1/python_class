ff6e25455b738981e109248867a63ed77e23e432
Eduardo Arrieta <61000997+eduardo-arrieta@users.noreply.github.com>
2022-04-07 00:29:16 -0500

    Nombre , cierre de archivo y orden
    Nombres de archivo no llevan espacios. El encabezado no requiere del caracter "#", con las triples comillas al inicio de este y al final está bien. Para abrir el archivo usas un open(), pero ya no lo cierras. Procura dividir tu código por bloques.

--- src/AT_y_CG porcentaje.py
+++ src/AT_y_CG porcentaje.py
@@ -1,22 +1,22 @@
-#'''NAME
-#AT y CG porcentaje
-#VERSION
-#1.1
-#AUTHOR
-#Andres Rivera Ramirez
+'''NAME
+AT y CG porcentaje
+VERSION
+1.1
+AUTHOR
+Andres Rivera Ramirez
 
-#DESCRIPTION
-#Programa para calcular el porcentaje de AT y CG de una secuencia dada por un archivo del cual se solicita su ruta
-#CATEGORY
+DESCRIPTION
+Programa para calcular el porcentaje de AT y CG de una secuencia dada por un archivo del cual se solicita su ruta
+CATEGORY
 
-#USAGE
-#Se inserta la ruta del archivo de texto plano que contiene la secuencia a analizar.
-#ARGUMENTS
+USAGE
+Se inserta la ruta del archivo de texto plano que contiene la secuencia a analizar.
+ARGUMENTS
 
-#SEE ALSO
+SEE ALSO
 
 
-#'''
+'''
 
 #La ruta del archivo se lee desde el teclado
 my_file_name = input('Inserte la ruta del archivo que contiene la secuencia de DNA incluyendo su nombre:\n')
@@ -24,14 +24,18 @@ my_file_name = input('Inserte la ruta del archivo que contiene la secuencia de D
 my_file = open(my_file_name)
 #Se lee el archivo
 my_file_content = my_file.read()
-dna = my_file_content
+dna = my_file_content # Esta variable esta siendo reduntante
+
 #Se obtiene la cantidad de nucleótidos, se asume que no hay \n u otro caracter no imprimible
 dna_lenght = len(dna)
 #Se suman las A y T
 AT_content = dna.count('A') + dna.count('T')
 #Se obtiene el porcentaje de AT
 AT_percentage = (AT_content)*100/(dna_lenght)
+
 print('Porcentaje de: \n')
 print("AT", AT_percentage, "%")
 #Se obtiene el porcentaje de GC a partir del porcentaje de AT
 print("CT", (100-AT_percentage), "%")
+
+my_file.close()

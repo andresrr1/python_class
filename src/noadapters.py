8cf15178070822108d7bda6621f33e6a2c4fff9e
Eduardo Arrieta <61000997+eduardo-arrieta@users.noreply.github.com>
2022-04-21 05:04:18 -0500

    Organización y te faltó un char
    Tu código está arrastrando algunos puntos malos. El principal problema es que para que funcione correctamente el usuario debe modificar los paths a los archivos, cosa que claramente está mal,  y esto se debe a que no estás proporcionando los archivos de entrada. Tienes que organizar tu repositorio agregando carpetas para `data` y `results`. Otro consejo es no usar rutas relativas, a menos que indiques el lugar exacto donde se debe correr tu programa, lo mejor será que indiques los paths como si siempre se corriese en la carpeta `python_class`. Tu encabezado puede mejorar. Separa por bloques tu código. No estás retirando 14 caracteres, solo quitas 13.

--- src/noadapters.py
+++ src/noadapters.py
@@ -1,32 +1,36 @@
-'''NAME
-    trimming
-    VERSION
+'''
+NAME
+    noadapters.py
+VERSION
     1.0
-    AUTHOR
+AUTHOR
     Andres Rivera Ramirez
 
-    DESCRIPTION
+DESCRIPTION
     Remover las secuencias adaptadoras
-    CATEGORY
+CATEGORY
 
-    USAGE
+USAGE
+    python noadapters.py
+ARGUMENTS
 
-    ARGUMENTS
-
-    SEE ALSO
+SEE ALSO
 
 
-    '''
+'''
 
 # Abrir el archivo
-input_adapters = ("..\data\input_adapters.txt")
+input_adapters = ("data\input_adapters.txt", 'r')
 input_adapters_content = open(input_adapters)
+
 # Leer el archivo por líneas
 secuencias = input_adapters_content.readlines()
 input_adapters_content.close()
+
 # Crear el archivo que contendrá el resultado
-output_noadapters = open("output_noadapters.txt", "a")
+output_noadapters = open("output_noadapters.txt", "w") # Si corres dos veces o más con 'a' de opción se irán anexando las impresiones
 # Quitar el adaptador de cada línea y hacer append al archivo
 for secuencia in secuencias:
-    output_noadapters.write(secuencia[13:])
+    output_noadapters.write(secuencia[14:])
+    
 output_noadapters.close()

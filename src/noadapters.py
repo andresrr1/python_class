566270cde3f907189b114276047dbbf663eeb1cd
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2022-05-02 21:06:17 -0500

    Excepciones y se arreglo el input y output

--- src/noadapters.py
+++ src/noadapters.py
@@ -2,35 +2,35 @@
 NAME
     noadapters.py
 VERSION
-    1.0
+    1.1
 AUTHOR
     Andres Rivera Ramirez
-
 DESCRIPTION
     Remover las secuencias adaptadoras
 CATEGORY
-
 USAGE
     python noadapters.py
 ARGUMENTS
-
 SEE ALSO
-
-
 '''
 
 # Abrir el archivo
-input_adapters = ("data\input_adapters.txt", 'r')
-input_adapters_content = open(input_adapters)
+try:
+    input_adapters = (".\data\input_adapters.txt")
+    input_adapters_content = open(input_adapters)
 
-# Leer el archivo por líneas
-secuencias = input_adapters_content.readlines()
-input_adapters_content.close()
+    # Leer el archivo por líneas
+    secuencias = input_adapters_content.readlines()
+    input_adapters_content.close()
 
-# Crear el archivo que contendrá el resultado
-output_noadapters = open("output_noadapters.txt", "w") # Si corres dos veces o más con 'a' de opción se irán anexando las impresiones
-# Quitar el adaptador de cada línea y hacer append al archivo
-for secuencia in secuencias:
-    output_noadapters.write(secuencia[14:])
-    
-output_noadapters.close()
+    # Crear el archivo que contendrá el resultado
+    output_noadapters = open(".\myresults\output_noadapters.txt", "w")
+
+    # Quitar el adaptador de cada línea y escribir en el archivo
+    for secuencia in secuencias:
+        output_noadapters.write(secuencia[14:])
+
+    output_noadapters.close()
+
+except IOError as ex:
+    print("No se pudo abrir el archivo: \n" + ex.strerror)

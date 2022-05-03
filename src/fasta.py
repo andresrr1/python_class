816a5caa406f82340f6e59cf547a5380a9ff4010
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2022-05-02 21:08:10 -0500

    Excepciones y se arreglo el output y el input

--- src/fasta.py
+++ src/fasta.py
@@ -1,36 +1,37 @@
 '''NAME
-Generador de fasta
+fasta
 VERSION
 1.1
 AUTHOR
 Andres Rivera Ramirez
-
 DESCRIPTION
 Se genera un archivo un fasta con la secuencia dentro de un archivo de texto llamado dna.txt
 CATEGORY
-
 USAGE
 Se requiere de un archivo dna.txt dentro de una carpeta data
 ARGUMENTS
-
 SEE ALSO
-
-
 '''
-#Se obtiene la ruta del archivo
-my_file_name = ("..\data\dna.txt")
-#Se abre el archivo para lectura
-my_file = open(my_file_name)
-#Se lee el archivo y se guarda su contenido en una variable
-my_file_content = my_file.read()
+# Se obtiene la ruta del archivo
+from typing import IO
 
-#Se crea el archivo fasta y se puede escribir en el
-my_file_two = open("dna.fasta", "w")
 
-#Se escribe en el archivo
-my_file_two.write(">sequence_name\n")
-my_file_two.write(my_file_content)
+try:
+    archivo_txt = (".\data\dna.txt")
+    # Se abre el archivo para lectura
+    my_file = open(archivo_txt)
+    # Se lee el archivo y se guarda su contenido en una variable
+    my_file_content = my_file.read()
 
-#Se cierra el archivo
-my_file_two.close()
-my_file.close()
+    # Se crea el archivo fasta y se puede escribir en el
+    archivo_fasta = open(".\myresults\dna.fasta", "w")
+
+    # Se escribe en el archivo
+    archivo_fasta.write(">sequence_name\n")
+    archivo_fasta.write(my_file_content)
+
+    # Se cierran los archivos
+    archivo_fasta.close()
+    my_file.close()
+except IOError as ex:
+    print("No es posible acceder al archivo" + ex.strerror)

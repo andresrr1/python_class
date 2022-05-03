f6664ced2d268f587dd88821078f0ef936967046
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2022-05-02 21:09:39 -0500

    Se agrego un raise por si el input es incorrecto

--- src/cuenta_de_nucleotidos.py
+++ src/cuenta_de_nucleotidos.py
@@ -1,7 +1,7 @@
 '''NAME
 cuenta de nucleotidos
 VERSION
-1.1
+1.2
 AUTHOR
 Andres Rivera Ramirez
 
@@ -17,9 +17,18 @@ SEE ALSO
 
 
 '''
-#La secuencia de DNA se lee desde el teclado
+# La secuencia de DNA se lee desde el teclado
 dna = input('Inserte secuencia de DNA:')
-#Se utilizan las llaves para hacer varias count en una sola línea de código.
+# Se verifica que la secuencia de DNA solo contenga letras mayusculas
+if(not dna.isupper()):
+    raise ValueError("La secuencia contiene letras minúsculas")
+# Se verifica que no existan numeros en la secuencia de DNA
+if(any(char.isdigit() for char in dna)):
+    raise ValueError("La secuencia contiene uno o mas números")
+# Se verifica que no existan caracteres que no correspondan a nucleotidos en la secuencia
+if(any(char != 'A' or 'C' or 'G' or 'T' for char in dna)):
+    raise ValueError("Se introdujo un caracter inadecuado en la secuencia")
+# Se utilizan las llaves para hacer varias count en una sola línea de código.
 print(f"El número de nucleotidos es \n \
 A:{dna.count('A')} \n C:{dna.count('C')} \n \
 T:{dna.count('T')} \n G:{dna.count('G')}")

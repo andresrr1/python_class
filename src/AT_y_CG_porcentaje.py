15ccda30a71565aef7c073513675a84a6c44d6c8
andresrr1 <100377641+andresrr1@users.noreply.github.com>
2022-05-11 22:17:37 -0500

    Se añadio el uso de argumentos

--- src/AT_y_CG_porcentaje.py
+++ src/AT_y_CG_porcentaje.py
@@ -1,29 +1,47 @@
 '''NAME
 AT_y_CG porcentaje
 VERSION
-1.1
+1.2
 AUTHOR
 Andres Rivera Ramirez
 DESCRIPTION
-Programa para calcular el porcentaje de AT y CG de una secuencia dada por un archivo del cual se solicita su ruta
+Programa para calcular el porcentaje de AT y CG de una secuencia dada por un archivo del cual se solicita su ruta como argumento a traves de la linea de comandos
 CATEGORY
 USAGE
-Se inserta la ruta del archivo de texto plano que contiene la secuencia a analizar.
+Se inserta la ruta del archivo de texto plano que contiene la secuencia a analizar como el argumento -i y el archivo de salida en el que se escribira el resultado como
+el argumento -o
 ARGUMENTS
 SEE ALSO
 '''
 
-# La ruta del archivo se lee desde el teclado
-path = input(
-    'Inserte la ruta del archivo que contiene la secuencia de DNA incluyendo su nombre:\n')
+import argparse
+from email import parser
+
+# Estos son los argumentos que requiere el programa, son paths a archivos de texto
+parser = argparse.ArgumentParser(
+    description=" Este programa calcula el porcentaje de AT y CG  en una secuencia utilizando argumentos de linea de comandos")
+
+parser.add_argument("-i", "--input",
+                    metavar="El path del archivo de entrada",
+                    help="Archivo de secuencias de DNA",
+                    required=True)
+
+parser.add_argument("-o", "--output",
+                    help="El path del archivo  de salida",
+                    required=False)
+
+
+args = parser.parse_args()
+
+
 # Se abre el archivo para leer
 try:
-    file = open(path)
+    file = open(args.input)
     # Se lee el archivo
     dna = file.read()
     file.close()
     # Se verifica que el archivo no contenga numeros, de lo contrario se notifica
-    if(any(char.isdigit for char in dna)):
+    if(any(char.isdigit() for char in dna)):
         raise ValueError("El archivo contiene uno o mas números")
 
     # Se obtiene la cantidad de nucleótidos, se asume que no hay \n u otro caracter no imprimible
@@ -32,11 +50,23 @@ try:
     AT_content = dna.count('A') + dna.count('T')
     # Se obtiene el porcentaje de AT
     AT_percentage = (AT_content) * 100 / (dna_lenght)
+    CG_percentage = 100 - AT_percentage
 
     print('Porcentaje de: \n')
     print("AT", AT_percentage, "%")
     # Se obtiene el porcentaje de GC a partir del porcentaje de AT
-    print("CT", (100 - AT_percentage), "%")
+    print("CG", CG_percentage, "%")
 # De no poder abrir o acceder al archivo se da la excepcion y se notifica cual fue el caso
 except IOError as ex:
     print("No es posible abrir el archivo: \n" + ex.strerror)
+
+# Se crea el archivo de salida y se guardan los porcentajes en el
+if(args.output):
+    try:
+        archivo_porcentaje = open(args.output, "w")
+        archivo_porcentaje.write("AT:\n")
+        archivo_porcentaje.write(str(AT_percentage))
+        archivo_porcentaje.write("\nCG:\n")
+        archivo_porcentaje.write(str(CG_percentage))
+    except IOError as ex:
+        print("No es posible crear el archivo de salida", ex.strerror)
